c96d1fe0d78bdb4c0267381d83fdeea97111ec7f
alberto <alberto@albertoX1>
2015-11-27 15:43:44 +0100

    timeline

--- arup_map/src/main/webapp/js/arup-timeline.js
+++ arup_map/src/main/webapp/js/arup-timeline.js
@@ -15,6 +15,11 @@ arup.TIMELINE = {
         this.labelTop = this.container.find('.label-top strong');
         this.labelBottom = this.container.find('.label-bottom strong');
         
+        this.constraintTop = $('<div style="position: absolute;" id="timelineConstraintTop"></div>');
+        $('body').append(this.constraintTop);
+        this.constraintBottom = $('<div style="position: absolute;" id="timelineConstraintBottom"></div>');
+        $('body').append(this.constraintBottom);
+        
         $.getJSON("conf", _.bind(function (d) {
             this.conf = d;
             this.render();
@@ -34,48 +39,113 @@ arup.TIMELINE = {
                 this.timeset.append(li);
             }
         }
+        var li = $('<li>' + obdobi[obdobi.length-1].do + '  <strong>-</strong></li>');
+        this.timeset.append(li);
+        
+        this.liHeight = $(this.timeset.find("li")[1]).offset().top - $(this.timeset.find("li")[0]).offset().top;
+        
         this.timeline.css("height", this.timeset.height());
-        this.timelineBottom.css("margin-top", this.timeset.height() - this.timelineTop.height()*2);
+        this.timelineBottom.css("top", this.timeset.height() - this.timelineTop.height()*2);
+        
+        
+        this.labelTop.text(this.conf.obdobi[0].od);
+        this.labelBottom.text(this.conf.obdobi[this.conf.obdobi.length - 1].do);
+        this.selDo[0].selectedIndex = this.conf.obdobi.length - 1;
+        
+        this.setConstraints();
+        
+        this.timelineBottom.draggable({ 
+            axis: "y" , 
+            containment: "#timelineConstraintBottom",
+            stop: function(){arup.TIMELINE.dragEndBottom();}
+        });
+        this.timelineTop.draggable({ 
+            axis: "y", 
+            containment: "#timelineConstraintTop",
+            stop: function(){arup.TIMELINE.dragEndTop();}
+        });
+
         this.selOd.on("change", _.bind(function(){
-            var idxOd = this.selOd[0].selectedIndex;
-            var idxDo = this.selDo[0].selectedIndex;
-            this.labelTop.text(this.conf.obdobi[idxOd].od);
-            
-            
-            var h = $(this.timeset.find("li")[1]).offset().top - $(this.timeset.find("li")[0]).offset().top;
-            var pos = h * Math.floor(idxOd / 3);
-            this.timelineTop.css("margin-top", pos);
-            
-            if(idxOd > idxDo){
-                this.selDo[0].selectedIndex = idxOd;
-                this.labelBottom.text(this.conf.obdobi[idxOd].do);
-                this.timelineBottom.css("margin-top", pos - parseInt(this.timelineTop.css("margin-top")));
-            }else{
-                var pos2 = h * Math.floor(idxDo / 3);
-                this.timelineBottom.css("margin-top", pos2 - pos);
-            }
+            this.setOdPos();
+            this.setConstraints();
             this.search();
-            
         }, this));
         
         this.selDo.on("change", _.bind(function(){
-            var idxOd = this.selOd[0].selectedIndex;
-            var idxDo = this.selDo[0].selectedIndex;
-            this.labelBottom.text(this.conf.obdobi[idxDo].do);
-            
-            var h = $(this.timeset.find("li")[1]).offset().top - $(this.timeset.find("li")[0]).offset().top;
-            var pos = h * Math.floor(idxDo / 3);
-            
-            if(idxDo < idxOd){
-                this.selOd[0].selectedIndex = idxDo;
-                this.labelTop.text(this.conf.obdobi[idxDo].od);
-                this.timelineTop.css("margin-top", pos);
-            }
-            
-            this.timelineBottom.css("margin-top", pos - parseInt(this.timelineTop.css("margin-top")));
+            this.setDoPos();
+            this.setConstraints();
             this.search();
-        
         }, this));
+    },
+    dragEndTop: function(){
+        this.posToObdobi();
+        this.setOdPos();
+        this.setConstraints();
+        this.search();
+    },
+    dragEndBottom: function(){
+        this.posToObdobi();
+        this.setDoPos();
+        this.setConstraints();
+        this.search();
+    },
+    posToObdobi: function(){
+        var pos = this.timelineTop.position().top;
+        var idxOd = pos * 3 / this.liHeight;
+        this.selOd[0].selectedIndex = idxOd;
+        
+        
+        var pos2 = this.timelineBottom.position().top;
+        var idxDo = pos2 * 3 / this.liHeight - 1;
+        this.selDo[0].selectedIndex = idxDo;
+        
+    },
+    setConstraints: function(){
+        this.constraintTop.width(this.timeline.width());
+        this.constraintTop.height(this.timelineBottom.offset().top - this.timeline.offset().top);
+        this.constraintTop.offset({
+            left:this.timeline.offset().left, 
+            top: this.timeline.offset().top
+        });
+        
+        this.constraintBottom.width(this.timeline.width());
+        this.constraintBottom.height(this.timeline.offset().top + this.timeline.height() - this.timelineTop.offset().top  - this.timelineTop.height());
+        this.constraintBottom.offset({
+            left:this.timeline.offset().left, 
+            top: this.timelineTop.offset().top + this.timelineTop.height()
+        });
+    },
+    setOdPos: function(){
+        var idxOd = this.selOd[0].selectedIndex;
+        var idxDo = this.selDo[0].selectedIndex;
+        this.labelTop.text(this.conf.obdobi[idxOd].od);
+
+        var pos = this.liHeight * idxOd / 3;
+        this.timelineTop.css("top", pos);
+
+        if(idxOd > idxDo){
+            this.selDo[0].selectedIndex = idxOd;
+            this.labelBottom.text(this.conf.obdobi[idxOd].do);
+            this.timelineBottom.css("top", pos);
+        }else{
+            //var pos2 = this.liHeight * idxDo / 3;
+            //this.timelineBottom.css("top", pos2);
+        }
+    },
+    setDoPos: function(){
+        var idxOd = this.selOd[0].selectedIndex;
+        var idxDo = this.selDo[0].selectedIndex;
+        this.labelBottom.text(this.conf.obdobi[idxDo].do);
+
+        var pos = this.liHeight * idxDo / 3;
+
+        if(idxDo < idxOd){
+            this.selOd[0].selectedIndex = idxDo;
+            this.labelTop.text(this.conf.obdobi[idxDo].od);
+            this.timelineTop.css("top", pos);
+        }
+
+        this.timelineBottom.css("top", pos - parseInt(this.timelineTop.css("top")));
     },
     search: function(){
         
