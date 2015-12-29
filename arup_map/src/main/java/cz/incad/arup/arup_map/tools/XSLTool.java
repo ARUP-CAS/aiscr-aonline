8ce37e4db19caff77ec62ee7333853c0a111acd2
alberto <alberto@albertoX1>
2015-12-29 12:37:01 +0100

    Customizable texts

--- arup_map/src/main/java/cz/incad/arup/arup_map/tools/XSLTool.java
+++ arup_map/src/main/java/cz/incad/arup/arup_map/tools/XSLTool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 alberto
+ * Copyright (C) 2015 alberto
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
